942472ec2a532049e8b6372aeae14c8ed9f5b9ca
LuisLunaDevelopment <34756706+LuisLunaDevelopment@users.noreply.github.com>
2017-12-22 15:34:13 -0800

    pie

---
+++ Ball Game/Assets/_Scripts/DestroyByTouch.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTouch : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter(Collider other){
+		FindObjectOfType<GameManager> ().EndGame();
+		Destroy(other.gameObject);		
+	}
+}
