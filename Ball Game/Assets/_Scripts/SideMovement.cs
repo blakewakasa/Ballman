dbae3e27ac2c9648d6801ca9887fad2991eed954
Blake Wakasa <bwakasa@uci.edu>
2017-12-22 16:27:26 -0800

    Change to SideMovement script

--- Ball Game/Assets/_Scripts/SideMovement.cs
+++ Ball Game/Assets/_Scripts/SideMovement.cs
@@ -6,7 +6,7 @@ public class SideMovement : MonoBehaviour {
 
 	[SerializeField]
 	private int sidespeed;
-
+    private Vector3 truespeed;
 	[SerializeField]
 	private int advance;
 
@@ -14,26 +14,34 @@ public class SideMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody> ();		
+		rb = GetComponent<Rigidbody> ();
+        
 	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		transform.Translate (Vector3.forward * advance * Time.deltaTime);
-        rb.velocity = new Vector3(0.0f,0.0f , advance);
 		
+        //rb.velocity = new Vector3(0.0f,0.0f , advance);
+        rb.velocity = new Vector3(0.0f, 0.0f, advance);
         
 
 		if (Input.GetKey(KeyCode.RightArrow))
 		{
-            
-            rb.velocity += (new Vector3(sidespeed, 0.0f, 0.0f));
+            /*if (rb.velocity.x < 0)
+            {
+                rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            }*/
+            rb.velocity = (new Vector3(sidespeed, 0.0f, advance));
 			//transform.Translate(sidespeed * Time.deltaTime, 0,0);
 		}
 
-		if (Input.GetKey(KeyCode.LeftArrow))
-		{
-            rb.velocity +=(new Vector3(-sidespeed, 0.0f, 0.0f));
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            /*if (rb.velocity.x > 0)
+            {
+                rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            }*/
+            rb.velocity =(new Vector3(-sidespeed, 0.0f, advance));
 			//transform.Translate(-sidespeed * Time.deltaTime, 0,0);
 		}
 
