30fb8a787c0a0310547d8edcc94addb051f91466
LuisLunaDevelopment <34756706+LuisLunaDevelopment@users.noreply.github.com>
2017-12-22 23:21:11 -0800

    level03 and moving plataforms

---
+++ Ball Game/Assets/_Scripts/HorizontalWall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalWall : MonoBehaviour {
+
+	public Transform movingPlatform;
+	public Transform position1;
+	public Transform position2;
+	public Vector3 newPosition;
+	public string state;
+	public float smooth;
+	public float resetTime;
+
+	void Start(){
+		ChangeTarget ();
+	}
+
+	void FixedUpdate(){
+		movingPlatform.position = Vector3.Lerp (movingPlatform.position, newPosition, smooth * Time.deltaTime);
+
+	}
+
+	void ChangeTarget(){
+		if (state == "Move1") {
+			state = "Move2";
+			newPosition = position2.position;
+		} else if (state == "Move2") {
+			state = "Move1";
+			newPosition = position1.position;
+		} else if (state == "") {
+			state = "Move2";
+			newPosition = position2.position;
+		}
+		Invoke ("ChangeTarget", resetTime);
+	}
+}
