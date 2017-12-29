d36a0902bbd7fb2d081422874169ae4438ab751a
Blake Wakasa <32974476+blakewakasa@users.noreply.github.com>
2017-12-28 21:30:49 -0800

    Fix option menu sound

---
+++ Ball Game/Assets/OptionsMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class OptionsMenu : MonoBehaviour {
+
+    
+    AudioSource audioSource;
+    /*void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+       
+        audioSource.volume = PlayerPrefs.GetFloat("SliderVolumeLevel", audioSource.volume);
+    }*/
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+    }
+}
--- Ball Game/Assets/_Scripts/MainMenu.cs
+++ Ball Game/Assets/_Scripts/MainMenu.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
     public void PlayGame()//starts the game at the first level
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
     public void QuitGame()
-        {
-            Application.Quit();
-        }
+    {
+        Application.Quit();
+    }
 }
